b4ffd5ab681c159f09bdfd2cda52731bbf2abcd4
Matias Tebele <mati.tebele@gmail.com>
2016-12-07 00:46:55 -0300

    Calculating word lengths.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         ProbabillityCounter probCounter = new ProbabillityCounter();
-        HashMap<Integer,Integer> probTable = probCounter.getProbabilityTable("testFile.txt");
+        HashMap<Integer, Integer> probTable = probCounter.getProbabilityTable("testFile.txt");
 
         for (int i = -1; i < 256; i++) {
-            System.out.println(i+" - "+probTable.get(i));
+            System.out.println(i + " - " + probTable.get(i));
         }
 
+        System.out.println("-----LONGITUDES-----");
+
+        HashMap<Integer, Integer> lengthTable = probCounter.getLengthTable(probTable);
+        for (int i = 0; i < 256; i++) {
+            if (lengthTable.get(i) != 0) {
+                System.out.println(i + " - " + lengthTable.get(i));
+            }
+        }
     }
 }
--- src/com/company/ProbabillityCounter.java
+++ src/com/company/ProbabillityCounter.java
@@ -1,21 +1,23 @@
 package com.company;
-import java.io.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class ProbabillityCounter {
 
-    public HashMap<Integer,Integer> getProbabilityTable(String filename) throws IOException {
+    public HashMap<Integer, Integer> getProbabilityTable(String filename) throws IOException {
 
         FileInputStream in = null;
 
         try {
             in = new FileInputStream(filename);
 
-            HashMap<Integer,Integer> probTable = new HashMap<Integer,Integer>();
+            HashMap<Integer, Integer> probTable = new HashMap<Integer, Integer>();
 
             probTable.put(-1, in.available());
             for (int i = 0; i < 256; i++) {
-                probTable.put(i,0);
+                probTable.put(i, 0);
             }
 
             int c;
@@ -25,11 +27,33 @@ public class ProbabillityCounter {
             }
 
             return probTable;
-
         } finally {
             if (in != null) {
                 in.close();
             }
         }
+    }
+
+    public HashMap<Integer, Integer> getLengthTable(HashMap<Integer, Integer> probTable) {
+
+        HashMap<Integer, Integer> lengthTable = new HashMap<Integer, Integer>();
+
+        int totalCount = probTable.get(-1);
+
+        for (int i = 0; i < probTable.size() - 1; i++) {
+            double probability = (double) probTable.get(i) / totalCount;
+            if (probability > 0) {
+                int length = -log(probability, 2);
+                lengthTable.put(i, length);
+            } else {
+                lengthTable.put(i, 0);
+            }
+        }
+
+        return lengthTable;
+    }
+
+    private static int log(double x, int base) {
+        return (int) (Math.log(x) / Math.log(base));
     }
 }
